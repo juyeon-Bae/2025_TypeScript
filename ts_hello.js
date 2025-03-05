70ca42ed6aaf855a88f45d56a085aa59e3885023
juyeonBae <w2316@e-mirim.hs.kr>
2025-03-05 09:29:06 +0900

    first commit

---
+++ ts_hello.js
@@ -0,0 +1,5 @@
+function tsAdd(x, y) {
+    return x + y;
+}
+var result1 = tsAdd(10, 20);
+console.log(result1);
---
+++ ts_hello.ts
@@ -0,0 +1,6 @@
+function tsAdd(x: number, y: number): number {
+    return x + y;
+}
+
+let result1 = tsAdd(10,20)
+console.log(result1);
